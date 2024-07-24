b9213425c9f7de7eabdedc779f47edf872b0aff7
moonlittarotgit <moonlit.tarot.company@gmail.com>
2024-07-24 09:03:12 +0530

    Update script.js
    spinner loader js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const countdownElement = document.getElementById('countdown');
     const notificationElement = document.getElementById('notification');
     const form = document.getElementById('subscription-form');
+    const spinner = document.getElementById('spinner');
 
     function updateCountdown() {
         const launchDate = new Date('August 15, 2024 00:00:00').getTime();
@@ -25,9 +26,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Form Submission Handling
     const scriptURL = 'https://script.google.com/macros/s/AKfycbzbLOe4KdyI7zpvZYNW280Z1xUaSahRgtCLXBeh3f4k02oHf9WyWxX8UdHqYYU6WwV3/exec';
-    
+
     form.addEventListener('submit', e => {
         e.preventDefault();
+
+        // Show the spinner
+        spinner.style.display = 'block';
 
         fetch(scriptURL, { 
             method: 'POST', 
@@ -41,6 +45,10 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch(error => {
             console.error('Error!', error.message);
             notificationElement.innerHTML = 'There was an error. Please try again.';
+        })
+        .finally(() => {
+            // Hide the spinner after the submission is complete
+            spinner.style.display = 'none';
         });
     });
 });
