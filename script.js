01f5b7e52b70b30dc633f9c0bb5cfc77d01b8548
moonlittarotgit <moonlit.tarot.company@gmail.com>
2024-08-14 00:53:21 +0530

    Update script.js
    time extend to 21aug.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const spinner = document.getElementById('spinner');
 
     function updateCountdown() {
-        const launchDate = new Date('August 15, 2024 00:00:00').getTime();
+        const launchDate = new Date('August 21, 2024 00:00:00').getTime();
         const now = new Date().getTime();
         const timeDifference = launchDate - now;
 
